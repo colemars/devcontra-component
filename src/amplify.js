6bca2a54166521fb9e915bab949a7c32af866658
Cole Marsteller <cgm32547@gmail.com>
2019-09-10 18:02:45 -0700

    build amplify helper

---
+++ src/amplify.js
@@ -0,0 +1,23 @@
+import Amplify from 'aws-amplify';
+import config from './config';
+
+export default async function configureAmplify() {
+  Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    },
+    API: {
+      endpoints: [
+        {
+          name: 'contra',
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION,
+        },
+      ],
+    },
+  });
+}
