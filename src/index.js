a2a8d451098c0a4020e6e9897691b7fdad3cf854
Cole Marsteller <cgm32547@gmail.com>
2019-09-23 22:03:52 -0700

    get data and display timeline

--- src/index.js
+++ src/index.js
@@ -1,42 +1,77 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-param-reassign */
+
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Auth, API } from 'aws-amplify';
 import configureAmplify from './amplify';
+import Timeline from './timeline';
 
 const DevContra = props => {
-  const { email, password } = props;
+  const { email, password, determinedMax, diameter, padding } = props;
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-    async function getAuth() {
+    async function getData() {
       try {
-        configureAmplify();
         const apiName = 'contra';
-        const path = '/profile/stackoverflow';
-        await Auth.signIn(email, password);
-        const stackOverflow = await API.get(apiName, path);
-        console.log(stackOverflow);
+        const stackPath = '/profile/stackoverflow';
+        const gitPath = '/profile/github';
+        const twitterPath = '/profile/twitter';
+        const [stackOverflow, github, twitter] = await Promise.all([
+          API.get(apiName, stackPath),
+          API.get(apiName, gitPath),
+          API.get(apiName, twitterPath),
+        ]);
+        return {
+          result: { stackOverflow, github, twitter },
+        };
       } catch (err) {
         console.log(err);
+        return {
+          error: err,
+        };
       }
     }
-    getAuth();
+    async function InitWithAuth() {
+      configureAmplify();
+      await Auth.signIn(email, password);
+      const { result, error } = await getData();
+      if (error) {
+        console.log(error);
+        return;
+      }
+      setData(result);
+    }
+    InitWithAuth();
   }, [email, password]);
-  console.log(email, password);
+
   return (
-    <div
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: 'blue',
-      }}
-    >
-      Hello World
-    </div>
+    <>
+      {data && (
+        <Timeline
+          data={data}
+          determinedMax={determinedMax}
+          diameter={diameter}
+          padding={padding}
+        />
+      )}
+    </>
   );
+};
+
+DevContra.defaultProps = {
+  diameter: 350,
+  padding: 0,
+  determinedMax: 0,
 };
 
 DevContra.propTypes = {
   email: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
+  diameter: PropTypes.number,
+  padding: PropTypes.number,
+  determinedMax: PropTypes.number,
 };
 
 export default DevContra;
