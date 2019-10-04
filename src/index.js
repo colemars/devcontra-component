1e82b096fda23a4bf5c6aa183df2dbcfb2ceae95
Cole Marsteller <cgm32547@gmail.com>
2019-10-03 18:19:02 -0700

    replace email and password with accessKey

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@
 
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Auth, API } from 'aws-amplify';
+import { API } from 'aws-amplify';
 import configureAmplify from './amplify';
 import Timeline from './timeline';
 
 const DevContra = props => {
-  const { email, password, determinedMax, diameter, padding } = props;
+  const { accessKey, determinedMax, diameter, padding } = props;
   const [data, setData] = useState(null);
 
   useEffect(() => {
     async function getData() {
       try {
         const apiName = 'contra';
-        const stackPath = '/profile/stackoverflow';
-        const gitPath = '/profile/github';
-        const twitterPath = '/profile/twitter';
+        const stackPath = `/profile/stackoverflow/${accessKey}`;
+        const gitPath = `/profile/github/${accessKey}`;
+        const twitterPath = `/profile/twitter/${accessKey}`;
         const [stackOverflow, github, twitter] = await Promise.all([
           API.get(apiName, stackPath),
           API.get(apiName, gitPath),
@@ -35,8 +35,7 @@ const DevContra = props => {
     }
     async function InitWithAuth() {
       configureAmplify();
-      await Auth.signIn(email, password);
-      const { result, error } = await getData();
+      const { result, error } = await getData(accessKey);
       if (error) {
         console.log(error);
         return;
@@ -44,7 +43,7 @@ const DevContra = props => {
       setData(result);
     }
     InitWithAuth();
-  }, [email, password]);
+  }, []);
 
   return (
     <>
@@ -67,8 +66,7 @@ DevContra.defaultProps = {
 };
 
 DevContra.propTypes = {
-  email: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
+  accessKey: PropTypes.string.isRequired,
   diameter: PropTypes.number,
   padding: PropTypes.number,
   determinedMax: PropTypes.number,
